67eae12c339f4e03a647955d472cf14396dee655
fatemeh.rezaei <rezaei.fatemeh92@gmail.com>
2024-08-11 20:44:58 +0430

    added header

---
+++ src/components/layout/Header.js
@@ -0,0 +1,28 @@
+import Link from "next/link";
+import { FiLogin } from "react-icons/fi";
+import styles from "@/layout/Header.module.css";
+
+function Header() {
+  return (
+    <header className={styles.header}>
+      <div>
+        <ul>
+          <li>
+            <Link href="/">صفحه اصلی</Link>
+          </li>
+          <li>
+            <Link href="/buy-residential">آگهی ها</Link>
+          </li>
+        </ul>
+      </div>
+      <div className={styles.login}>
+        <Link href="/signin">
+          <FiLogin />
+          <span>ورود</span>
+        </Link>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
