42055d44b8affde3cbcfc6d74e83fb59d9517e86
fatemeh.rezaei <rezaei.fatemeh92@gmail.com>
2024-08-11 21:43:03 +0430

    added signup api

---
+++ src/app/api/auth/signup/route.js
@@ -0,0 +1,46 @@
+import { NextResponse } from "next/server";
+import User from "@/models/User";
+import connectDB from "@/utils/connectDB";
+import { hashPassword } from "@/utils/auth";
+
+export async function POST(req) {
+  try {
+    await connectDB();
+
+    const { email, password } = await req.json();
+    console.log({ email, password });
+
+    if (!email || !password) {
+      return NextResponse.json(
+        { error: "لطفا اطلاعات معتبر وارد کنید" },
+        { status: 422 }
+      );
+    }
+
+    const existingUser = await User.findOne({ email });
+    console.log("existingUser: ", existingUser);
+
+    if (existingUser) {
+      return NextResponse.json(
+        { error: "این حساب کاربری وجود دارد" },
+        { status: 422 }
+      );
+    }
+
+    const hashedPassword = await hashPassword(password);
+
+    const newUser = await User.create({
+      email,
+      password: hashedPassword,
+    });
+    console.log("newUser", newUser);
+
+    return NextResponse.json({ message: "حساب کاربری ایجاد شد" });
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json(
+      { error: "مشکلی در سرور رخ داده است" },
+      { status: 500 }
+    );
+  }
+}
