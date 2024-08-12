841c5be7c3b4f1cab5ce32994cdadde150b4302f
fatemeh.rezaei <rezaei.fatemeh92@gmail.com>
2024-08-12 17:41:14 +0430

    added sign in page

---
+++ src/app/signin/page.js
@@ -0,0 +1,9 @@
+import SigninPage from "@/template/signinPage"
+
+function Signin() {
+  return (
+    <SigninPage />
+  )
+}
+
+export default Signin
---
+++ src/components/template/signinPage.js
@@ -0,0 +1,79 @@
+"use client";
+
+import Link from "next/link";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { signIn } from "next-auth/react";
+import toast, { Toaster } from "react-hot-toast";
+import { ThreeDots } from "react-loader-spinner";
+import styles from "@/template/SignupPage.module.css";
+
+function SigninPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const router = useRouter();
+
+  const signinHandler = async (e) => {
+    e.preventDefault();
+
+    setLoading(true);
+
+    const res = await signIn("credentials", {
+      email,
+      password,
+      redirect: false,
+    });
+
+    setLoading(false);
+
+    if (res.error) {
+      toast.error(res.error);
+    } else {
+      router.push("/");
+    }
+  };
+
+  return (
+    <div className={styles.form}>
+      <h4>فرم ورود</h4>
+      <form>
+        <label htmlFor="email">ایمیل:</label>
+        <input
+          type="text"
+          id="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <label htmlFor="password">رمز عبور:</label>
+        <input
+          type="password"
+          id="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        {loading ? (
+          <ThreeDots
+            color="#304ffe"
+            height={45}
+            ariaLabel="three-dots-loading"
+            visible={true}
+            wrapperStyle={{ margin: "auto" }}
+          />
+        ) : (
+          <button type="submit" onClick={signinHandler}>
+            ثبت نام
+          </button>
+        )}
+      </form>
+      <p>
+        حساب کاربری ندارید؟
+        <Link href="/signup">ثبت نام</Link>
+      </p>
+      <Toaster />
+    </div>
+  );
+}
+
+export default SigninPage;
