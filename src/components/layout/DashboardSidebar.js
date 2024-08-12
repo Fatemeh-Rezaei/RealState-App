14557688a614e40be99fe1d69fee46ce351ea5cc
fatemeh.rezaei <rezaei.fatemeh92@gmail.com>
2024-08-12 20:46:48 +0430

    added user panel

---
+++ src/components/layout/DashboardSidebar.js
@@ -0,0 +1,26 @@
+import Link from "next/link";
+import { getServerSession } from "next-auth";
+import { CgProfile } from "react-icons/cg";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import LogoutButton from "@/module/LogoutButton";
+import styles from "@/layout/DashboardSidebar.module.css";
+
+async function DashboardSidebar({ children }) {
+  const session = await getServerSession(authOptions);
+  return (
+    <div className={styles.container}>
+      <div className={styles.sidebar}>
+        <CgProfile />
+        <p>{session?.user.email}</p>
+        <span></span>
+        <Link href="/dashboard">حساب کاربری</Link>
+        <Link href="/dashboard/my-profiles">آگهی های من</Link>
+        <Link href="/dashboard/add">ثبت آگهی</Link>
+        <LogoutButton />
+      </div>
+      <div className={styles.main}>{children}</div>
+    </div>
+  );
+}
+
+export default DashboardSidebar;
