1a3b0c937d0836c04ba23caf2e81a241211fa353
fatemeh.rezaei <rezaei.fatemeh92@gmail.com>
2024-08-14 19:26:12 +0430

    added delete ad

--- src/components/module/DashboardCard.js
+++ src/components/module/DashboardCard.js
@@ -5,6 +5,7 @@ import { AiOutlineDelete } from "react-icons/ai";
 import { FiEdit } from "react-icons/fi";
 import Card from "@/module/Card";
 import styles from "@/module/DashboardCard.module.css";
+import toast, { Toaster } from "react-hot-toast";
 
 function DashboardCard({ data }) {
   const router = useRouter();
@@ -12,7 +13,18 @@ function DashboardCard({ data }) {
     router.push(`/dashboard/my-profiles/${data._id}`);
   };
 
-  const deleteHandler = () => {};
+  const deleteHandler = async () => {
+    const res = await fetch(`/api/profile/delete/${data._id}`, {
+      method: "DELETE",
+    });
+    const result = await res.json();
+    if (result.error) {
+      toast.error(result.error);
+    } else {
+      toast.success(result.message);
+      router.refresh()
+    }
+  };
 
   return (
     <div className={styles.container}>
@@ -26,6 +38,7 @@ function DashboardCard({ data }) {
           حذف آگهی <AiOutlineDelete />
         </button>
       </div>
+      <Toaster />
     </div>
   );
 }
